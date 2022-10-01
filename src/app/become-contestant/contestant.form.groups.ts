c36bf19a340f1bf6e95cb77c5774713c7622be02
Eric Renken <eric@mynoc.com>
2022-10-01 00:27:38 -0400

    Added little miss forms (#38)

--- src/app/become-contestant/contestant.form.groups.ts
+++ src/app/become-contestant/contestant.form.groups.ts
@@ -8,4 +8,9 @@ export class ContesantFormGroup {
 var documentUrl = "../../assets/documents/contestant";
 
 export const ContesantFormGroups:  ContesantFormGroup[] = [
+    { header: "Little Miss", formItems: [ 
+        { displayName: "Little Miss Junior Teen Teen Bridgman Practice Schedule", link: `${documentUrl}/2023 LM JR Teen Teen Bridgman Practice Schedule.docx` },
+        { displayName: "Little Miss Contestant Application", link: `${documentUrl}/2023 Little Miss Contestant Application.docx` },
+        { displayName: "Little Miss Contestant Biography", link: `${documentUrl}/2023 Little Miss Contestant Biography.doc` }
+    ] }
 ];
