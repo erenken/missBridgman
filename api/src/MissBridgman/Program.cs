1baadf87cac63224e24d2df0a340ecf9ec56ebda
Eric Renken <eric@mynoc.com>
2024-03-22 22:52:14 -0400

    Update function to .NET 8

--- api/src/MissBridgman/Program.cs
+++ api/src/MissBridgman/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.Hosting;
 using MissBridgman.Repositories;
 
 var host = new HostBuilder()
-	.ConfigureFunctionsWorkerDefaults()
+	.ConfigureFunctionsWebApplication()
 	.ConfigureServices(services =>
 	{
 		services.AddSingleton<IStorage, Storage>();
